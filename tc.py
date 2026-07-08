9b9cb3d310a4b8cdd27789abf9f77529c90f8795
s6906022610401-alt <s690602261040@email.kmutnb.ac.th>
2026-07-08 09:19:29 +0700

    tc

--- tc.py
+++ tc.py
@@ -1,8 +1,13 @@
 # Declaring variables
 age = 25
 height = float(age)
+print("Height",height)
+
 height = 5.6
 age = int(height)
+print("Age",age)
+
 num_str ="123"
 num = int(num_str)
+print("number",num_str)
 
