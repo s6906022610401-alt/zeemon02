2ffc55e84aaf97d1f5cf02d6c4de6b01da280747
s6906022610401-alt <s690602261040@email.kmutnb.ac.th>
2026-07-08 09:51:08 +0700

    ngi

---
+++ ngi.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+income = float(input("Enter your income: "))
+print("Here is the data you entered:")
+print("Name:", name)
+print("Age:", age)
+print("Income:", income)
