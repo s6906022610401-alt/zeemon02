d6fb14faf124ffea13e4287c0b19984a2d4efdc7
s6906022610401-alt <s690602261040@email.kmutnb.ac.th>
2026-07-15 08:45:06 +0700

    exe

---
+++ exe.py
@@ -0,0 +1,7 @@
+score1 = int(input ("Enter score test 1: "))
+score2 = int(input ("Enter score test 2: "))
+score3 = int(input ("Enter score test 3: "))
+total = (score1 + score2 + score3) /3
+print("The average score is ",total)
+if total>95:
+    print("Congratulations!")
---
+++ exo.py
@@ -0,0 +1,3 @@
+calsius = float(input("enter temperature in calsius"))
+fahrenheit = (calsius*9/5)+32
+print ("tmperrature in {fahrenheit} is fahrenheit.")
