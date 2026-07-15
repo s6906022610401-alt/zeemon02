f086b4a4063f0a16d009ff69fc83aa4b426e195e
s6906022610401-alt <s690602261040@email.kmutnb.ac.th>
2026-07-15 09:29:43 +0700

    ands

--- ands.py
+++ ands.py
@@ -1,7 +1,8 @@
 num = float(input("enter a number: "))
 if num > 0:
-    print("Positive Number")
-elif num == 0:
-    print("Zero")
+    if num == 0:
+        print("Zero")
+    else:
+        print("Positive Number")
 else:
     print("Negative number")
